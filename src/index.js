46d0207c7ca7c551c5f492580611230d90a46a2a
brenna987 <63932067+brenna987@users.noreply.github.com>
2020-05-24 15:36:44 -0700

    Added temperature conversion function

--- src/index.js
+++ src/index.js
@@ -56,6 +56,24 @@ function handleSubmit(event) {
   let cityInputElement = document.querySelector("#city-search-input");
   search(cityInputElement.value);
 }
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
 //current location
 
 function showLocationWeather(position) {
@@ -88,5 +106,11 @@ form.addEventListener("submit", handleSubmit);
 
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
 let apiKey = "4b72c972f7597913fe5a676591dd7b39";
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Los Angeles");
